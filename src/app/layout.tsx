0946f72f1b624868b433dddf70dbb9e381252b47
dun <dun@macbook.pro>
2024-12-16 23:40:32 +0800

    feat: add buymeacoffe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="laduniestu"
+          data-description="Support me on Buy me a coffee!"
+          data-message="☕ buy me a coffe"
+          data-color="#FF813F"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+          defer
+        />
+      </head>
       <body className={`antialiased`}>
         <ThemeProvider
           attribute="class"
